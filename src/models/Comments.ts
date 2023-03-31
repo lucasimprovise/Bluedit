fd91abc6ae45e62de571e2ca41648f2b7a862a8a
RyanPeyrot <ryan.peyrot@gmail.com>
2023-03-31 18:11:54 +0200

    Update des models, ajout de fonctionnalité

--- src/models/Comments.ts
+++ src/models/Comments.ts
@@ -1,20 +1,24 @@
-import firestore from '@react-native-firebase/firestore';
-import {User} from './User';
-import {Post} from './Post';
-const COMMENTS = firestore().collection('Comments');
+import firestore from "@react-native-firebase/firestore";
+import { User } from "./User";
+import { Post } from "./Post";
+const COMMENTS = firestore().collection("Comments");
 
 export class Comments {
-  id: string | undefined;
-  author: User;
-  date: Date;
-  parentPost: Post | Comments;
-  content: string;
+  id?: string;
+  author?: User;
+  date?: Date;
+  parentPost?: Post | Comments;
+  content?: string;
 
-  constructor(author: User, parentPost: Post | Comments, content: string) {
+  private constructor() {}
+
+  createCommentObj(author: User, parentPost: Post | Comments, content: string) {
     this.author = author;
     this.parentPost = parentPost;
     this.content = content;
     this.date = new Date();
+
+    return this;
   }
 
   addComments() {
@@ -25,7 +29,7 @@ export class Comments {
       content: this.content,
     })
       .then((docRef: any) => {
-        console.log('Document ajouté avec ID: ', docRef.id);
+        console.log("Document ajouté avec ID: ", docRef.id);
       })
       .catch((error: any) => {
         console.error("Erreur lors de l'ajout du document: ", error);
@@ -36,21 +40,28 @@ export class Comments {
     COMMENTS.doc(id)
       .delete()
       .then((docRef: any) => {
-        console.log('Document supprimer : ', docRef);
+        console.log("Document supprimer : ", docRef);
       })
       .catch((error: any) => {
         console.log("Erreur lors de la suppression de l'objet", error);
       });
   }
 
-  static getComment(id: string) {
+  static getComment(id: string): Comments {
     COMMENTS.doc(id)
       .get()
-      .then(documentSnapshot => {
-        console.log('User exists: ', documentSnapshot.data());
+      .then((documentSnapshot) => {
+        console.log("User exists: ", documentSnapshot.data());
+        let comment: Comments = Object.assign(
+          new Comments(),
+          documentSnapshot.data()
+        );
+        comment.id = id;
+        return comment;
       })
       .catch((error: any) => {
-        console.log('Erreur lors de la recuperation via ID', error);
+        console.log("Erreur lors de la recuperation via ID", error);
       });
+    return new Comments();
   }
 }
--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -1,12 +1,15 @@
-import firestore from '@react-native-firebase/firestore';
-const TAGS = firestore().collection('Tags');
+import firestore from "@react-native-firebase/firestore";
+const TAGS = firestore().collection("Tags");
 
 export class Tag {
   id: string | undefined;
-  title: string;
+  title?: string;
 
-  constructor(title: string) {
+  private constructor() {}
+
+  public createTagObj(title: string) {
     this.title = title;
+    return this;
   }
 
   addTag() {
@@ -14,7 +17,7 @@ export class Tag {
       title: this.title,
     })
       .then((docRef: any) => {
-        console.log('Document ajouté avec ID: ', docRef.id);
+        console.log("Document ajouté avec ID: ", docRef.id);
       })
       .catch((error: any) => {
         console.error("Erreur lors de l'ajout du document: ", error);
@@ -25,7 +28,7 @@ export class Tag {
     TAGS.doc(id)
       .delete()
       .then((docRef: any) => {
-        console.log('Document supprimer : ', docRef);
+        console.log("Document supprimer : ", docRef);
       })
       .catch((error: any) => {
         console.log("Erreur lors de la suppression de l'objet", error);
@@ -35,24 +38,28 @@ export class Tag {
   static getTag(id: string) {
     TAGS.doc(id)
       .get()
-      .then(documentSnapshot => {
-        console.log('Tag exists: ', documentSnapshot.data());
+      .then((documentSnapshot) => {
+        console.log("Tag exists: ", documentSnapshot.data());
+        let tag: Tag = Object.assign(new Tag(), documentSnapshot.data());
+        tag.id = documentSnapshot.id;
+        return tag;
       })
       .catch((error: any) => {
-        console.log('Erreur lors de la recuperation via ID', error);
+        console.log("Erreur lors de la recuperation via ID", error);
       });
+    return new Tag();
   }
 
   static getTagByTitle(field: string) {
-    TAGS.where('username', '==', field)
+    TAGS.where("username", "==", field)
       .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(documentSnapshot => {
-          console.log('Tag ID: ', documentSnapshot.id, documentSnapshot.data());
+      .then((querySnapshot) => {
+        querySnapshot.forEach((documentSnapshot) => {
+          console.log("Tag ID: ", documentSnapshot.id, documentSnapshot.data());
         });
       })
       .catch((error: any) => {
-        console.log('Erreur lors de la recuperation via ID', error);
+        console.log("Erreur lors de la recuperation via ID", error);
       });
   }
 }
