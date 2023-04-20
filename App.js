84342e363d23e476f9e8f5bb833bfe9b7b7e67e5
Marvin GOMES VITORINO <56402327+Gomerinho@users.noreply.github.com>
2023-04-20 15:25:19 +0200

    [ADD] Animation community

--- App.js
+++ App.js
@@ -1,63 +1,95 @@
-import React, { createContext, useMemo, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { Provider } from 'react-redux'
-import HomePage from './src/screens/HomePage'
-import { store } from './src/store/store'
-import MyProfile from './src/screens/MyProfile'
-import Login from './src/screens/Login'
-import Register from './src/screens/Register'
-import Settings from './src/screens/Settings'
-import './src/config/translation'
-import SplashScreen from './src/components/SplashScreen'
-import CreatePostScreen from './src/screens/Post/CreatePostScreen'
-import PostDetailScreen from './src/screens/Post/PostDetailScreen'
-import CommunityDetailScreen from './src/screens/Community/CommunityDetail'
+import React, { createContext, useMemo, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import HomePage from './src/screens/HomePage';
+import { store } from './src/store/store';
+import MyProfile from './src/screens/MyProfile';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
+import Settings from './src/screens/Settings';
+import './src/config/translation';
+import SplashScreen from './src/components/SplashScreen';
+import CreatePostScreen from './src/screens/Post/CreatePostScreen';
+import PostDetailScreen from './src/screens/Post/PostDetailScreen';
+import CommunityDetailScreen from './src/screens/Community/CommunityDetail';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const screenOptions = {
-  headerShown: false
-}
+  headerShown: false,
+};
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
 const App = () => {
-  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [isSignedIn, setIsSignedIn] = useState(false);
   const appContextValue = useMemo(
     () => ({
       isSignedIn,
-      setIsSignedIn
+      setIsSignedIn,
     }),
     [isSignedIn]
-  )
+  );
 
   return (
     <AppContext.Provider value={appContextValue}>
       <Provider store={store}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="SplashScreen">
-            <Stack.Screen name="SplashScreen" options={screenOptions}>
-              {(props) => <SplashScreen {...props} isSignedIn={isSignedIn} />}
+          <Stack.Navigator initialRouteName='SplashScreen'>
+            <Stack.Screen name='SplashScreen' options={screenOptions}>
+              {props => <SplashScreen {...props} isSignedIn={isSignedIn} />}
             </Stack.Screen>
-            <Stack.Screen name="Login" component={Login} options={screenOptions} />
-            <Stack.Screen name="Register" component={Register} options={screenOptions} />
+            <Stack.Screen
+              name='Login'
+              component={Login}
+              options={screenOptions}
+            />
+            <Stack.Screen
+              name='Register'
+              component={Register}
+              options={screenOptions}
+            />
 
             {isSignedIn && (
               <>
-                <Stack.Screen name="HomePage" component={HomePage} options={screenOptions} />
-                <Stack.Screen name="Settings" component={Settings} options={screenOptions} />
-                <Stack.Screen name="MyProfile" component={MyProfile} options={screenOptions} />
-                <Stack.Screen name="CreatePost" component={CreatePostScreen} options={screenOptions} />
-                <Stack.Screen name="PostDetail" component={PostDetailScreen} options={screenOptions} />
-                <Stack.Screen name='CommunityDetail' component={CommunityDetailScreen} options={screenOptions}/>
+                <Stack.Screen
+                  name='HomePage'
+                  component={HomePage}
+                  options={screenOptions}
+                />
+                <Stack.Screen
+                  name='Settings'
+                  component={Settings}
+                  options={screenOptions}
+                />
+                <Stack.Screen
+                  name='MyProfile'
+                  component={MyProfile}
+                  options={screenOptions}
+                />
+                <Stack.Screen
+                  name='CreatePost'
+                  component={CreatePostScreen}
+                  options={screenOptions}
+                />
+                <Stack.Screen
+                  name='PostDetail'
+                  component={PostDetailScreen}
+                  options={screenOptions}
+                />
+                <Stack.Screen
+                  name='CommunityDetail'
+                  component={CommunityDetailScreen}
+                  options={screenOptions}
+                />
               </>
             )}
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
     </AppContext.Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, TextInput } from 'react-native'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import styled from 'styled-components/native'
-import { useNavigation } from '@react-navigation/native'
-import { useTranslation } from 'react-i18next'
-import ProfileScreen from './MyProfile'
-import Login from './Login'
-import { useSelector, useDispatch } from 'react-redux'
-import AuthOrProfile from './../components/AuthOrProfile'
-import logo from './../assets/bluedit-logo.png'
-import { getPostsRequest } from '../store/actions/post'
-import Post from '../components/Post'
-import TranslateAnimation from '../components/TranslateAnimation'
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, TextInput } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import styled from 'styled-components/native';
+import { useNavigation } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
+import ProfileScreen from './MyProfile';
+import Login from './Login';
+import { useSelector, useDispatch } from 'react-redux';
+import AuthOrProfile from './../components/AuthOrProfile';
+import logo from './../assets/bluedit-logo.png';
+import { getPostsRequest } from '../store/actions/post';
+import Post from '../components/Post';
+import TranslateAnimation from '../components/TranslateAnimation';
 
 const fakeCommunities = [
   { id: '1', name: 'r/ReactNative' },
   { id: '2', name: 'r/JavaScript' },
-  { id: '3', name: 'r/Python' }
-]
+  { id: '3', name: 'r/Python' },
+];
 
 const communities = [
   { id: '1', name: 'b/React' },
@@ -44,36 +44,25 @@ const fakePosts = [
     title: 'React Native is amazing!',
     author: 'JohnDoe',
     community: 'r/ReactNative',
-    upvotes: 340
+    upvotes: 340,
   },
   {
     id: '2',
     title: 'JavaScript vs TypeScript',
     author: 'JaneDoe',
     community: 'r/JavaScript',
-    upvotes: 275
+    upvotes: 275,
   },
   {
     id: '3',
     title: 'Python for Data Science',
     author: 'DataMaster',
     community: 'r/Python',
-    upvotes: 420
-  }
-]
+    upvotes: 420,
+  },
+];
 
-const TopTab = createMaterialTopTabNavigator()
-
-const CommunitiesScreen = (props) => {
-  const { t } = useTranslation()
-  const [searchValue, setSearchValue] = useState('')
-  const handleSearch = (value) => {
-    setSearchValue(value)
-  }
-
-  const filteredCommunities = fakeCommunities.filter((community) => {
-    return community.name.toLowerCase().includes(searchValue.toLowerCase())
-  })
+const TopTab = createMaterialTopTabNavigator();
 
 const CommunitiesScreen = () => {
   const navigation = useNavigation();
@@ -82,54 +71,61 @@ const CommunitiesScreen = () => {
       <FlatList
         data={communities}
         keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <CommunityContainer
-            onPress={() => {
-              navigation.navigate('CommunityDetail', {
-                communityName: item.name.substring(2),
-              });
-            }}
-          >
-            <CommunityName>{item.name}</CommunityName>
-          </CommunityContainer>
+        renderItem={({ item, index }) => (
+          <TranslateAnimation delay={200 * index} duration={500}>
+            <CommunityContainer
+              onPress={() => {
+                navigation.navigate('CommunityDetail', {
+                  communityName: item.name.substring(2),
+                });
+              }}
+            >
+              <CommunityName>{item.name}</CommunityName>
+            </CommunityContainer>
+          </TranslateAnimation>
         )}
       />
     </View>
-  )
-}
+  );
+};
 
-const PopularScreen = (props) => {
-  const { t } = useTranslation()
-  const { posts } = useSelector((state) => state.post)
-  const navigation = useNavigation()
-  const [searchValue, setSearchValue] = useState('')
-  const dispatch = useDispatch()
+const PopularScreen = props => {
+  const { t } = useTranslation();
+  const { posts } = useSelector(state => state.post);
+  const navigation = useNavigation();
+  const [searchValue, setSearchValue] = useState('');
+  const dispatch = useDispatch();
 
-  const handleSearch = (value) => {
-    setSearchValue(value)
-  }
+  const handleSearch = value => {
+    setSearchValue(value);
+  };
 
-  const filteredPosts = posts.filter((post) => {
-    return post.title.toLowerCase().includes(searchValue.toLowerCase())
-  })
+  const filteredPosts = posts.filter(post => {
+    return post.title.toLowerCase().includes(searchValue.toLowerCase());
+  });
 
   useEffect(() => {
-    dispatch(getPostsRequest())
-  }, [dispatch])
+    dispatch(getPostsRequest());
+  }, [dispatch]);
 
   return (
     <View>
-      <SearchBar onChangeText={handleSearch} value={searchValue} placeholder={t('search_posts')} />
+      <SearchBar
+        onChangeText={handleSearch}
+        value={searchValue}
+        placeholder={t('search_posts')}
+      />
       {props.navigation?.isFocused() && (
         <FlatList
           data={filteredPosts}
-          keyExtractor={(item) => item.id}
+          keyExtractor={item => item.id}
           renderItem={({ item, index }) => (
             <TranslateAnimation delay={200 * index} duration={500}>
               <PostContainer
                 onPress={() => {
-                  navigation.navigate('PostDetail', { postId: item.id })
-                }}>
+                  navigation.navigate('PostDetail', { postId: item.id });
+                }}
+              >
                 <Post {...item}></Post>
               </PostContainer>
             </TranslateAnimation>
@@ -137,32 +133,38 @@ const PopularScreen = (props) => {
         />
       )}
     </View>
-  )
-}
+  );
+};
 
 const HomeTabs = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
   const topTabStyles = {
     activeTintColor: '#ffffff',
     inactiveTintColor: '#aaaaaa',
     style: {
-      backgroundColor: '#1e90ff'
-    }
-  }
+      backgroundColor: '#1e90ff',
+    },
+  };
 
   return (
     <TopTab.Navigator screenOptions={topTabStyles}>
-      <TopTab.Screen children={(props) => <CommunitiesScreen {...props} />} name={t('communities')} />
-      <TopTab.Screen children={(props) => <PopularScreen {...props} />} name={t('popular')} />
+      <TopTab.Screen
+        children={props => <CommunitiesScreen {...props} />}
+        name={t('communities')}
+      />
+      <TopTab.Screen
+        children={props => <PopularScreen {...props} />}
+        name={t('popular')}
+      />
     </TopTab.Navigator>
-  )
-}
+  );
+};
 
-export const BottomTab = createBottomTabNavigator()
+export const BottomTab = createBottomTabNavigator();
 
 const HomePage = () => {
-  const currentUser = useSelector((state) => state.currentUser)
-  const navigation = useNavigation()
+  const currentUser = useSelector(state => state.currentUser);
+  const navigation = useNavigation();
 
   // useEffect(() => {
   //   // Cette fonction sera exécutée lorsque currentUser change
@@ -170,64 +172,72 @@ const HomePage = () => {
   // }, [currentUser, navigation])
 
   const handleCreate = () => {
-    navigation.navigate('CreatePost')
-  }
+    navigation.navigate('CreatePost');
+  };
 
   const RedirectToProfileOrAuth = () => {
     if (currentUser) {
-      return MyProfile
+      return MyProfile;
     } else {
-      return Login
+      return Login;
     }
-  }
+  };
 
   const screenOptions = {
-    headerShown: false
-  }
+    headerShown: false,
+  };
 
   return (
     <Container>
       <Logo source={logo} />
-      <BottomTab.Navigator initialRouteName="Home">
-        <BottomTab.Screen name="Home" options={screenOptions} component={HomeTabs} />
-        <BottomTab.Screen name="User" options={screenOptions} component={ProfileScreen} />
+      <BottomTab.Navigator initialRouteName='Home'>
+        <BottomTab.Screen
+          name='Home'
+          options={screenOptions}
+          component={HomeTabs}
+        />
+        <BottomTab.Screen
+          name='User'
+          options={screenOptions}
+          component={ProfileScreen}
+        />
       </BottomTab.Navigator>
       <CreateButton onPress={handleCreate}>
         <CreateButtonText>+</CreateButtonText>
       </CreateButton>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.View`
   flex: 1;
   background-color: #ffffff;
-`
+`;
 
 const Logo = styled.Image`
   width: 150px;
   height: 50px;
   resize-mode: contain;
   margin: 10px auto;
-`
+`;
 
 const CommunityContainer = styled.TouchableOpacity`
   padding: 10px;
   border-bottom-width: 1px;
   border-bottom-color: #d3d3d3;
-`
+`;
 
 const CommunityName = styled.Text`
   font-size: 18px;
   font-weight: bold;
   color: #333;
-`
+`;
 
 const PostContainer = styled.TouchableOpacity`
   padding: 15px;
   border-bottom-width: 1px;
   border-bottom-color: #d3d3d3;
-`
+`;
 
 const CreateButton = styled.TouchableOpacity`
   position: absolute;
@@ -247,18 +257,18 @@ const CreateButton = styled.TouchableOpacity`
   }
   shadow-opacity: 0.25;
   shadow-radius: 3.84px;
-`
+`;
 
 const CreateButtonText = styled.Text`
   color: #ffffff;
   font-size: 32px;
   font-weight: bold;
-`
+`;
 
 const SearchBar = styled.TextInput`
   height: 40px;
   border-color: gray;
   border-width: 1px;
-`
+`;
 
-export default HomePage
+export default HomePage;
